bfd912f3ae01880a7e75ae89e1d5508033cc2ffc
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-16 15:53:07 +0000

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,8 @@ $username = "root";
 $password = "root";
 $dbname = "GERT";
 
+$username = $_POST['username'];
+
 $conn = new mysqli($servername, $username, $password, $dbname);
   
 if ($conn->connect_error) {
@@ -17,7 +19,7 @@ $result = $conn->query($sqlQueryDetails);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo $row["Username"]. $row["Password"].$row["Email"].$row["UserID"];
+    echo $row["Username"]. $row["Password"].$row["Email"].$row["UserID"].$username ;
   }
 } else {
   echo "0 results";
