99c25944f329178eab3ed560bf494b01f83f1d40
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-16 14:03:21 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,2 +1,16 @@
 <php>
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "GERT";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+  
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+  
+$sqlQueryDetails = "SELECT * FROM `Users`;
+$result = $conn->query($sqlQueryDetails);
+return $result;
 </php>
