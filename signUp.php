6f84a8787c3d94944af98cb364b858a3945151cd
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-03-18 14:47:32 +0000

    Update signUp.php

--- signUp.php
+++ signUp.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
   
 $sqlQueryDetails = "SELECT * FROM `Users`";
 
-$sqlInsert = "INSERT INTO `Users` (`Username`, `Password`, `Email`, `UserID`) VALUES (`$uName`, `$pWord`, `$email`, `3`);";
+$sqlInsert = "INSERT INTO `Users` (`Username`, `Password`, `Email`, `UserID`) VALUES ('$uName', '$pWord', '$email', '3');";
 $result = $conn->query($sqlQueryDetails);
 $usernameMatch = false;
 $emailMatch = false;
