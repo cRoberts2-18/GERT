df217bcfb526164a664644f933dd91e5da241eb0
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-18 11:41:39 +0000

    Create signUp.php

---
+++ signUp.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "GERT";
+
+$uName = $_POST['uName'];
+$pWord = $_POST['pWord'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+  
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+  
+$sqlQueryDetails = "SELECT * FROM `Users`";
+$result = $conn->query($sqlQueryDetails);
+
+  
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+  
+  }
+echo("working");
+?>
