8cf73feaacc6f4e20534024ae8191261a6009f42
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-18 13:32:21 +0000

    Update signUp.php

--- signUp.php
+++ signUp.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
   
 $sqlQueryDetails = "SELECT * FROM `Users`";
 $sqlInsert = "INSERT INTO Users (".$uName.",".$password.","$email."7)";
-VALUES ('John', 'Doe', 'john@example.com')";
+
 $result = $conn->query($sqlQueryDetails);
 $usernameMatch = false;
 $emailMatch = false;
@@ -43,7 +43,7 @@ if($pWord == $pWordCheck){
   }
   if(!$emailMatch && !$usernameMatch){
     
-    if ($conn->query($sqlInsert) === true) {
+    if ($conn->query($sqlInsert) == true) {
       echo "user created";
     }
   }
