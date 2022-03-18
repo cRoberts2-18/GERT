b4216bb571cd4a0316d98a6f873356d72c19c58a
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-03-18 12:54:49 +0000

    Update GertHome.php

--- GertHome.php
+++ GertHome.php
@@ -87,7 +87,8 @@ $(function() {
   		success: function(data) {
     		}
 });
-  });
+});
+});
   
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
