094e13a9fb87f1480ac2ab70efec0d474a427045
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-18 12:15:38 +0000

    Update signUp.php

--- signUp.php
+++ signUp.php
@@ -22,7 +22,7 @@ $result = $conn->query($sqlQueryDetails);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "workpls";
+    echo $email.$username.$pWord.$pWordCheck;
   }
 }
 ?>
