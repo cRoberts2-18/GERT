cde51dade297ea73b99217cd099e708096e6ccd7
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-03-18 15:44:32 +0000

    Update login.php

--- login.php
+++ login.php
@@ -21,10 +21,10 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     if (strtolower($row["Username"])== strtolower($uName) && $row["Password"] == $pWord){
+      echo "true";
       session_start();
       $_SESSION["GERTloggedin"] = true;
       $_SESSION["Username"] = $row["Username"];
-      echo "true";
       }
     else{
       echo "false";
