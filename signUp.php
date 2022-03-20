403de102e9e77cbb28f25f78eb92f0651f3b932c
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-20 23:45:50 +0000

    Update signUp.php

--- signUp.php
+++ signUp.php
@@ -41,7 +41,7 @@ if(strlen($pWord) < 8 || strlen($pWord) > 16){
   exit();
 }
 
-if(preg_match("/[A-Z]/" , $pWord)){
+if(preg_match("/[A-Z]/" , $pWord) && preg_match("/[a-z]/" , $pWord) ){
   echo("Password Good");
   exit();
 }
