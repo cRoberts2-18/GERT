12e99df09b1edefa3bba58606b06fb744ca21a06
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-03-21 03:28:02 +0000

    Update ResetLink.php

--- ResetLink.php
+++ ResetLink.php
@@ -12,45 +12,11 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$select=mysql_query("SELECT * FROM `Users` WHERE `Email` = '$email'");
+$select=mysql_query("SELECT * FROM `Users` WHERE `Email` = '$email';");
 if(mysql_num_rows($select)==1)
   {
-    while($row=mysql_fetch_array($select))
-    {
-      $email=md5($row['Email']);
-      $pass=md5($row['Password']);
-    }
-    $link="<a href='www.samplewebsite.com/reset.php?key=".$email."&reset=".$pass."'>Click To Reset password</a>";
-    require_once('phpmail/PHPMailerAutoload.php');
-    $mail = new PHPMailer();
-    $mail->CharSet =  "utf-8";
-    $mail->IsSMTP();
-    // enable SMTP authentication
-    $mail->SMTPAuth = true;                  
-    // GMAIL username
-    $mail->Username = "GERTool@gmail.com";
-    // GMAIL password
-    $mail->Password = "Gert_123";
-    $mail->SMTPSecure = "ssl";  
-    // sets GMAIL as the SMTP server
-    $mail->Host = "smtp.gmail.com";
-    // set the SMTP port for the GMAIL server
-    $mail->Port = "465";
-    $mail->From='GERT@gmail.com';
-    $mail->FromName='GERT';
-    $mail->AddAddress($email, $email);
-    $mail->Subject  =  'Reset Password';
-    $mail->IsHTML(true);
-    $mail->Body    = 'Click On This Link to Reset Password '.$link.'';
-    if($mail->Send())
-    {
-      echo "Check Your Email and Click on the link sent to your email";
-    }
-    else
-    {
-      echo "Mail Error - >".$mail->ErrorInfo;
-    }
-  }	
+  echo $row['Email'];   
+}
 
 
 
