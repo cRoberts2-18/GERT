541db52b56c128d0f1b70518508ef93d69255903
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-03-21 03:31:09 +0000

    Update ResetLink.php

--- ResetLink.php
+++ ResetLink.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$select=mysql_query("SELECT * FROM `Users` WHERE `Email` = '$email';");
+$select=mysql_query("SELECT * FROM `Users` WHERE `Email` = 'admin@admin.co.uk';");
 if(mysql_num_rows($select)==1)
   {
   echo $row['Email'];   
