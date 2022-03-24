8f2112d295fd9f7e28fb337059fc53841c04a0e8
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-24 21:00:01 +0000

    Update GertHome.php

--- GertHome.php
+++ GertHome.php
@@ -13,8 +13,8 @@ else if(isset($_SESSION["GERTloggedin"])!== true){
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <style>
 #map {
-	width: 1400px;
-	height: 500px;
+	width: 1350px;
+	height: 450px;
 }
 </style>
 <head>
