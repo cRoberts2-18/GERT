70efdbaab19cae9f50b081e770c34703c484ce9c
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-03-24 20:25:51 +0000

    Update GertHome.php

--- GertHome.php
+++ GertHome.php
@@ -13,7 +13,13 @@ else if(isset($_SESSION["GERTloggedin"])!== true){
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <head>
 <link rel="stylesheet" href="GERT.css">
-<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">#
+ <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" 
+       integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
 <title>GERT - Home</title>
 </head>
 <header>
@@ -79,6 +85,7 @@ else if(isset($_SESSION["GERTloggedin"])!== true){
 </body>
 
 <iframe src="placeholderMap.png" class="map"></iframe>
+<div id="map"></div>
 
 
 <script>
