24c8ccf00b09f72862ac6e75153665e9a9b2a00d
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-03-27 18:50:59 +0100

    Create AccDetails.php

---
+++ AccDetails.php
@@ -0,0 +1,36 @@
+<html translate="no">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="Scripts/notify.js"></script>
+<style>
+#map {
+	position: absolute;
+	top:102px;
+	left:0;
+	height: 85%;
+	width: 100%;
+	z-index:0;
+	opacity: .99;
+}
+</style>
+<head>
+<link rel="stylesheet" href="GERT.css">
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<link
+      rel="stylesheet"
+      href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+      integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+      crossorigin=""
+    />
+
+    <script
+      src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+      integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+      crossorigin=""
+    ></script>
+    
+    <header>
+    </header>
+    
+    <body>
+    Acc Details
+    </body>
