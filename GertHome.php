baf83267a3d71b9c810e287176c52afb70acf718
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-04-01 20:46:23 +0100

    Update GertHome.php

--- GertHome.php
+++ GertHome.php
@@ -126,9 +126,10 @@ function notifyMessage(msg,styles){
 
     var map = L.map('map',{
     center: [52.770775, -1.2043467],
+    zoomControl: false
     zoom: 15
-    new L.Control.Zoom({ position: 'topright' }).addTo(map);
     });
+    new L.Control.Zoom({ position: 'topright' }).addTo(map);
 
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
