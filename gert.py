e5fed4e189321a6f4b1e53ea2e8548f11674dbc3
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-04-04 11:46:27 +0100

    Update gert.py

--- gert.py
+++ gert.py
@@ -4,12 +4,21 @@ app= Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/profile/')
 def profile():
     return render_template('profile.html')
+
+@app.route('/login/')
+def loginPage():
+    return render_template('login.html')
+
+@app.route('/process/login', methods = ['POST'])
+def login():
+    uName = request.json['uName']
+    password = request.json['pWord']
+    return(uName + password)
+
 if __name__ == '__main__':
     app.run(debug=True)
-@app.route('/process/login', methods = ['POST'])
-    
-
     
