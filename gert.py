bb19020aedd37da4ae969dfa59480f431afea0cb
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-04-04 16:47:10 +0100

    Update gert.py

--- gert.py
+++ gert.py
@@ -24,7 +24,7 @@ def loginPage():
 @app.route('/processLogin/', methods = ['POST'])
 
 def processLogin():
-    data = request.get_data()
+    data = request.get_data('uName')
     return(data)
 
 @app.route('/signUp.php')
