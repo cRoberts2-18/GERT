ec0015bc359d298666735a469ad4436d5e2a1f6a
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-04-04 16:53:33 +0100

    Update ResetLink.php

--- OldGERT/ResetLink.php
+++ OldGERT/ResetLink.php
@@ -1,9 +1,10 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
 
 $servername = "localhost";
 $username = "root";
