f315feacf3d37d9a063282d07f3a2f99feeb0ba2
m-leadbetter <58077804+m-leadbetter@users.noreply.github.com>
2022-04-05 17:17:21 +0100

    Update gert.py

--- gert.py
+++ gert.py
@@ -39,7 +39,8 @@ def processLogin():
     myresult = mycursor.fetchall()
     
     for row in myresult:
-        if request.data('uName') == 
+        if request.value.get('uName') == row[1]:
+            return('true')
     
     return(myresult[1])
 
