88c1487782cfe90f7599fe8cbd857045e0d8db21
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-04-07 22:31:16 +0100

    Update gert.py

--- gert.py
+++ gert.py
@@ -107,10 +107,7 @@ def processAPICall():
     maximum=0
     for i in result:
         maximum= float(i[0])
-    if maximum is none:
-        SaveID=0
-    else:
-        SaveID=maximum+1
+    SaveID=maximum+1
     sql="INSERT INTO SavedSearches (SearchID, Name, OwnerID, Filename) VALUES (%s,%s,%s,%s);"
     values=(SaveID,fileName,session['uid'],SaveName)
     mycursor.execute(sql, values)
