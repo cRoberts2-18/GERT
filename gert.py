1bc634381a61e941ce6a9a7beba91755ddcdb26d
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-04-10 19:12:34 +0100

    Update gert.py

--- gert.py
+++ gert.py
@@ -96,7 +96,7 @@ def searchData():
     
     path=request.values.get('path')
     grbs=pygrib.open(path)
-    grb=grbs.read()[0]
+    grb=grbs.read()[1]
     data=grb.data(lat1=lat1,lat2=lat2,lon1=long1,lon2=long2)
     
     return str(grb)
